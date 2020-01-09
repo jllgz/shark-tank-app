587578fcf9d59d859d83642b91dea166bfd605f5
Joseph Obligar <you@example.com>
2020-01-08 23:23:23 -0800

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sta-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'shark-tank-app';
+}
